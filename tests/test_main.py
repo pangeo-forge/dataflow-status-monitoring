c89cbeb7ddc7b13bf8bd9807bb8077f6843bac0e
sharkinsspatial <sharkinsgis@gmail.com>
2022-06-16 11:13:09 -0500

    Fix test formatting.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-import os
 import base64
 import json
+import os
 from unittest.mock import patch
+
 from src.main import post_status
 
 pat = "pat"
@@ -12,35 +13,25 @@ repo = "repo"
 @patch("src.main.requests")
 @patch.dict(
     os.environ,
-    {
-        "PAT": pat,
-        "REPO_ORG": org,
-        "REPO": repo
-    },
+    {"PAT": pat, "REPO_ORG": org, "REPO": repo},
 )
 def test_post_status(requests):
     job_name = "test"
     alert = {
         "incident": {
-            "resource": {
-                "labels": {
-                    "job_name": job_name
-                }
-            },
-            "policy_name": "Dataflow Succeeded Alert"
+            "resource": {"labels": {"job_name": job_name}},
+            "policy_name": "Dataflow Succeeded Alert",
         }
     }
     alert_bytes = json.dumps(alert).encode("utf-8")
     alert_encoded = base64.b64encode(alert_bytes)
-    event = {
-        "data": alert_encoded
-    }
+    event = {"data": alert_encoded}
     post_status(event, {})
     requests.post.assert_called_once_with(
         f"https://api.github.com/repos/{org}/{repo}/dispatches",
         data=f'{{"client_payload": {{"flow_run_name": "{job_name}", "state": "Success"}}}}',
         headers={
             "Authorization": f"token {pat}",
-            "Accept": "application/vnd.github.v3+json"
-        }
+            "Accept": "application/vnd.github.v3+json",
+        },
     )
