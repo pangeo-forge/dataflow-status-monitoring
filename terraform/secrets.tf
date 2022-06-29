9d46b64532abaecefb2b5188439cda267d0d6401
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-06-29 14:59:17 -0700

    add secrets.tf

---
+++ terraform/secrets.tf
@@ -0,0 +1,14 @@
+resource "google_secret_manager_secret" "secret-basic" {
+  secret_id = "github_repository_dispatch_pat-latest"
+
+  replication {
+    automatic = true
+  }
+}
+
+
+resource "google_secret_manager_secret_version" "secret-version-basic" {
+  secret = google_secret_manager_secret.secret-basic.id
+
+  secret_data = "${var.pat}"
+}
