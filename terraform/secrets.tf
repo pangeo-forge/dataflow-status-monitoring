8bc47a3f277545be0904d44b0d392c35e4eb1281
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-06-29 17:00:37 -0700

    add secretsAccessor binding for runtime service account

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -10,9 +10,16 @@ resource "google_secret_manager_secret" "secret-basic" {
   }
 }
 
-
 resource "google_secret_manager_secret_version" "secret-version-basic" {
   secret = google_secret_manager_secret.secret-basic.id
 
   secret_data = var.pat
+}
+
+resource "google_secret_manager_secret_iam_binding" "binding" {
+  secret_id = google_secret_manager_secret.secret-basic.secret_id
+  role = "roles/secretmanager.secretAccessor"
+  members = [
+    "serviceAccount:${var.project}@appspot.gserviceaccount.com",
+  ]
 }
