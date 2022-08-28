9e6f5819baf090218f145ddd1e9c7737b0d1c6e3
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-08-27 22:19:00 -0700

    rename log sink

--- terraform/log_sink.tf
+++ terraform/log_sink.tf
@@ -1,5 +1,5 @@
 resource "google_logging_project_sink" "logsink" {
-  name                   = "logsink"
+  name                   = "github-app-logsink"
   destination            = "pubsub.googleapis.com/projects/${var.project}/topics/${resource.google_pubsub_topic.topic.name}"
   filter                 = "resource.type = dataflow_step AND logName=(\"projects/${var.project}/logs/dataflow.googleapis.com%2Fjob-message\") AND ((severity=ERROR AND textPayload=~\"^Workflow failed.*\") OR (severity=DEBUG AND textPayload=~\"^Executing success step success.*$\"))"
   unique_writer_identity = true
